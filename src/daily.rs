d4095c4728b40c373db0b5487d1edd0ae51ae13a
l-const <const-l@outlook.com>
2021-04-27 13:31:39 +0300

    init lib

---
+++ src/daily.rs
@@ -0,0 +1,5 @@
+/// Daily recorded events
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+extern crate serde;
+extern crate serde_json;
+extern crate ureq;
+
+use serde::{Deserialize, Serialize};
+
+const BASE_URL: &'static str = "https://covid-19-greece.herokuapp.com";
+
+#[derive(Deserialize, Debug)]
+struct Deaths {
+    cases: Vec<DeathSlot>,
+}
+
+#[derive(Deserialize, Debug)]
+struct DeathSlot {
+    deaths: u32,
+    date: String,
+}
+
+fn main() -> Result<(), ureq::Error> {
+    println!("Hello from main");
+    let body: String = ureq::get(format!("{}/deaths", BASE_URL).as_str())
+        .set("Accept", "application/json")
+        .call()?
+        .into_string()?;
+    // println!("Request body: {:?}", &body);
+    let deaths_res: Deaths = serde_json::from_str(&body).unwrap();
+
+    println!("{:?}", &deaths_res.cases[0]);
+    Ok(())
+}
