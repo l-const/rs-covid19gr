88f872364aa6c95a6f8e9eed62d6d0e12f55069f
l-const <const-l@outlook.com>
2021-04-28 13:29:41 +0300

    refug test patch, lib ipdate

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub mod refug;
 /// Define custom Result
 type Result<T> = std::result::Result<T, CrateError>;
 
+// API BASE_URL
+const BASE_URL: &'static str = "https://covid-19-greece.herokuapp.com";
+
 /// Represents different type of errors that can happen.
 #[derive(Debug, Clone, PartialEq)]
 enum CrateError {
@@ -14,4 +17,14 @@ enum CrateError {
     SerialError,
     /// The error was caused during an HTTP GET request.
     HttpError,
+}
+
+pub(crate) fn build_request(url_suffix: &str) -> String {
+    let body: String = ureq::get(format!("{}/{}", BASE_URL, url_suffix).as_str())
+        .set("Accept", "application/json")
+        .call()
+        .expect("http error")
+        .into_string()
+        .expect("serializaton error");
+    body
 }
--- src/refug.rs
+++ src/refug.rs
@@ -2,12 +2,24 @@
 
 use serde::{Deserialize, Serialize};
 
+use crate::build_request;
+use crate::BASE_URL;
+
 // HTTP GET /regugee-camps
 /// Recorded cases in Refugee-camps.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct RefCamps {
     #[serde(rename = "refugee-camps")]
     pub refugee_camps: Vec<RefCamp>,
+}
+
+impl IntoIterator for RefCamps {
+    type Item = RefCamp;
+    type IntoIter = std::vec::IntoIter<RefCamp>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.refugee_camps.into_iter()
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -26,7 +38,7 @@ pub struct RefCamp {
     pub region_en: String,
     pub region_gr: String,
     pub total_confirmed_cases: u32,
-    pub total_samples: u32,
+    pub total_confirmed_samples: u32,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -37,10 +49,62 @@ pub struct RecordedEvent {
     pub samples: Option<u32>,
 }
 
+pub fn get_refugee_camp_data() -> RefCamps {
+    let json_resp = build_request("refugee-camps");
+    let ref_camps = serde_json::from_str(&json_resp).unwrap();
+    ref_camps
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
+    const str_json: &str = r#"{
+              "refugee-camps":
+              [
+                               {
+                                "area_type_en": "string",
+                                "area_type_gr": "string",
+                                "capacity": 0,
+                                "current_hosts": 0,
+                                "description": "string",
+                                "last_update": "2021-04-28",
+                                "latitude": 0,
+                                "longtitude": 0,
+                                "name_en": "string",
+                                "name_gr": "string",
+                                "recorded_events": [
+                                    {
+                                    "case_detection_week": "string",
+                                    "confirmed_cases": 0,
+                                    "quarantine_duration_days": 0,
+                                    "samples": 0
+                                    }
+                                ],
+                                "region_en": "string",
+                                "region_gr": "string",
+                                "total_confirmed_cases": 0,
+                                "total_confirmed_samples": 0
+                                }
+                 ]
+                            }"#;
+
     #[test]
-    fn test_des_refugee() {}
+    fn test_deserilize_refugee() {
+        let ref_camps: Result<RefCamps, _> = serde_json::from_str(str_json);
+        assert!(ref_camps.is_ok());
+        //println!("RefCamps: {:?}", &ref_camps);
+    }
+
+    #[test]
+    fn test_get_refugee_camp_data() {}
+
+    #[test]
+    fn test_iterator() {
+        let ref_camps: RefCamps = serde_json::from_str(str_json).unwrap();
+        ref_camps
+            .into_iter()
+            .for_each(|ref_camp| println!("{:?}", ref_camp));
+    }
 }
