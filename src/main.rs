86286f58f256a95896631f4790f8b6c6d23d3406
l-const <const-l@outlook.com>
2021-05-01 14:06:20 +0300

    added example in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
-extern crate serde;
-extern crate serde_json;
-extern crate ureq;
-
+use rs_covid19gr::daily::*;
 use serde::{Deserialize, Serialize};
-
 const BASE_URL: &'static str = "https://covid-19-greece.herokuapp.com";
 
-#[derive(Deserialize, Serialize, Debug)]
-struct Deaths {
-    cases: Vec<DeathSlot>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct DeathSlot {
-    deaths: u32,
-    date: String,
-}
-
-fn main() -> Result<(), ureq::Error> {
+fn main() {
     println!("Hello from main");
-    let body: String = ureq::get(format!("{}/deaths", BASE_URL).as_str())
-        .set("Accept", "application/json")
-        .call()?
-        .into_string()?;
-    // println!("Request body: {:?}", &body);
-    let deaths_res: Deaths = serde_json::from_str(&body).unwrap();
-
-    println!("{:?}", &deaths_res.cases[0]);
-    Ok(())
+    let total_tests_series = get_total_tests_series_data();
+    let iter_len = total_tests_series.iter().len();
+    let first_iter = total_tests_series.iter().skip(1);
+    let second_iter = total_tests_series.iter().take(iter_len - 1 as usize);
+    let zip_iter = first_iter.zip(second_iter);
+    let iter_dates: Vec<&str> = total_tests_series
+        .iter()
+        .skip(1)
+        .map(|x| x.date.as_str())
+        .collect();
+    let result: Vec<i64> = zip_iter
+        .map(|(x, y)| x.tests.unwrap_or(0) as i64 - y.tests.unwrap_or(0) as i64)
+        .collect();
+    result
+        .iter()
+        .zip(iter_dates.iter())
+        .for_each(|(&x, &y)| println!("Diff: {:?} Date: {:?}", x, y));
 }
